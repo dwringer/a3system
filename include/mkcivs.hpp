6ff47080855314e86c0637a51656b7cc3fa49965
Darren Ringer <dwringer@gmail.com>
2016-10-19 22:43:26 -0400

    tweak mkcivs import sequence

--- include/mkcivs.hpp
+++ include/mkcivs.hpp
@@ -1,9 +1,10 @@
-fnc_mkcivs = compile preprocessfile "mkcivs\fnc_mkcivs.sqf";
-fnc_civLeaders = compile preprocessfile "mkcivs\fnc_civLeaders.sqf";
-
 /* Initialize ambient civilians: */
 civArray = [];
 civKilled = 0;
 Sentinel_civTriggers = false;
 Sentinel_mkCivs = false;
 Index_civGroups = 0;
+
+fnc_mkcivs = compile preprocessfile "mkcivs\fnc_mkcivs.sqf";
+fnc_civLeaders = compile preprocessfile "mkcivs\fnc_civLeaders.sqf";
+
