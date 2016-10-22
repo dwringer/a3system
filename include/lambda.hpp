779cf154533812915d65c748024075db4ba8f80d
Darren Ringer <dwringer@gmail.com>
2016-10-21 21:13:14 -0400

    added two new experimental functions (untested)

--- include/lambda.hpp
+++ include/lambda.hpp
@@ -1,1 +1,2 @@
 fnc_lambda = compile preprocessfile "lambda\fnc_lambda.sqf";
+fnc_map = compile preprocessfile "lambda\fnc_map.sqf";
--- include/vectools.hpp
+++ include/vectools.hpp
@@ -1,3 +1,4 @@
 fnc_shuffle = compile preprocessfile "vectools\fnc_shuffle.sqf";
 fnc_choose = compile preprocessfile "vectools\fnc_choose.sqf";
 fnc_sorted = compile preprocessfile "vectools\fnc_sorted.sqf";
+fnc_subseq = compile preprocessfile "vectools\fnc_subseq.sqf";
