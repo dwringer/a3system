a4d1587f5d9475e0b6412b8f804dd63f862bafda
Darren Ringer <dwringer@gmail.com>
2016-10-23 01:13:03 -0400

    added function def

--- include/lambda.hpp
+++ include/lambda.hpp
@@ -1,4 +1,5 @@
 fnc_lambda = compile preprocessfile "lambda\fnc_lambda.sqf";
 fnc_map = compile preprocessfile "lambda\fnc_map.sqf";
+fnc_mapwith = compile preprocessfile "lambda\fnc_mapwith.sqf";
 fnc_reduce = compile preprocessfile "lambda\fnc_reduce.sqf";
 fnc_filter = compile preprocessfile "lambda\fnc_filter.sqf";
