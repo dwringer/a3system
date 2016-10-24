5a2e26484791926f99dac63cf3725401749a20bc
Darren Ringer <dwringer@gmail.com>
2016-10-24 02:00:08 -0400

    added items method to dictionary

--- classdef/Dictionary.hpp
+++ classdef/Dictionary.hpp
@@ -24,3 +24,13 @@
  [["_o"],
   "_o getVariable ""__keys__"""
   ] call fnc_lambda] call fnc_method;
+
+["Dictionary", "items",
+ [["_o"],
+  "private [""_keys, _acc""];
+   _keys = [_o, ""keys""] call fnc_tell;
+   _acc = [[[""_k""], 
+           ""[_o, """"get"""", _k] call fnc_tell""] call fnc_lambda,
+           _keys] call fnc_map;
+   [_keys, _acc] call fnc_zip"
+  ] call fnc_lambda] call fnc_method;
