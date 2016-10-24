59013df5e52a018c4915f68e4e427923c1015192
Darren Ringer <dwringer@gmail.com>
2016-10-24 03:41:42 -0400

    further cleanup of class defs

--- classdef/Dictionary.hpp
+++ classdef/Dictionary.hpp
@@ -1,45 +1,36 @@
-["Dictionary",
- [["_o"],
-  {
-	  _o setVariable ["__keys__", []];
-	  _o
-	  
+["Dictionary", [["_o"], {
+	_o setVariable ["__keys__", []];
+	_o
 }] call fnc_lambda2] call fnc_class;
 
 
-["Dictionary", "get",
- [["_o", "_key"],
-  {
-	  _o getVariable [_key, nil]
-}] call fnc_lambda2] call fnc_method;
-
-["Dictionary", "set",
- [["_o", "_key", "_value"],
-  {
-	  private ["_keys"];
-	  _keys = _o getVariable "__keys__";
-	  if (({_x == _key} count _keys) == 0) then {
-			  _keys = _keys + [_key];
-			  _o setVariable ["__keys__", _keys];
-	  };
-	  _o setVariable [_key, _value]
-}] call fnc_lambda2] call fnc_method;
-
-["Dictionary", "keys",
- [["_o"],
-  {
-	  _o getVariable "__keys__"
+["Dictionary", "get", [["_o", "_key"], {
+        _o getVariable [_key, nil]
 }] call fnc_lambda2] call fnc_method;
 
 
-["Dictionary", "items",
- [["_o"],
-  {
-	  private ["_keys", "_acc"];
-	  _keys = [_o, "keys"] call fnc_tell;
-	  _acc = [[["_k"], 
-		  {[_o, "get", _k] call fnc_tell}] call fnc_lambda2,
-		  _keys] call fnc_map;
-	  [_keys, _acc] call fnc_zip
+["Dictionary", "set", [["_o", "_key", "_value"], {
+        private ["_keys"];
+	_keys = _o getVariable "__keys__";
+	if (({_x == _key} count _keys) == 0) then {
+	        _keys = _keys + [_key];
+		_o setVariable ["__keys__", _keys];
+	};
+	_o setVariable [_key, _value]
+}] call fnc_lambda2] call fnc_method;
+
+
+["Dictionary", "keys", [["_o"], {
+        _o getVariable "__keys__"
+}] call fnc_lambda2] call fnc_method;
+
+
+["Dictionary", "items", [["_o"], {
+        private ["_keys", "_acc"];
+	_keys = [_o, "keys"] call fnc_tell;
+	_acc = [[["_k"], 
+		{[_o, "get", _k] call fnc_tell}] call fnc_lambda2,
+		_keys] call fnc_map;
+	[_keys, _acc] call fnc_zip
 }] call fnc_lambda2] call fnc_method;
 
--- classdef/mkcivsWeaponCache.hpp
+++ classdef/mkcivsWeaponCache.hpp
@@ -1,14 +1,12 @@
 WeaponCaches = [];
 
-["WeaponCache",
- [["_o"],
-  {
-	  _o spawn {
-		  WeaponCaches = WeaponCaches + [_this];
-	  };
-	  str _o
+["WeaponCache", [["_o"], {
+        _o spawn {
+		WeaponCaches = WeaponCaches + [_this];
+        };
+	str _o
 }] call fnc_lambda2] call fnc_class;
 
-["WeaponCache",
- "positionString",
- [["_o"], {str position _o}] call fnc_lambda2] call fnc_method;
+["WeaponCache", "positionString", [["_o"], {
+        str position _o
+}] call fnc_lambda2] call fnc_method;
