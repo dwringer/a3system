5c592a34e3195ad16bf8ca63c7cc93d4eeed7e2b
Darren Ringer <dwringer@gmail.com>
2016-10-25 00:38:56 -0400

    added fnc_new to class header

--- include/classes.hpp
+++ include/classes.hpp
@@ -1,5 +1,6 @@
 fnc_class = compile preprocessfile "classes\fnc_class.sqf";
 fnc_instance = compile preprocessfile "classes\fnc_instance.sqf";
+fnc_new = compile preprocessfile "classes\fnc_new.sqf";
 fnc_method = compile preprocessfile "classes\fnc_method.sqf";
 fnc_tell = compile preprocessfile "classes\fnc_tell.sqf";
 
