8457889df04da9e181dba4a3dfa1d114d296999d
Darren Ringer <DRinger@DARREN-POB.domain.pob>
2016-10-27 08:58:52 -0400

    rearranged dependencies for better separation

--- include/randint.hpp
+++ include/randint.hpp
@@ -3,4 +3,6 @@
 fnc_logarithm = compile preprocessfile "randint\fnc_logarithm.sqf";
 fnc_bits2int = compile preprocessfile "randint\fnc_bits2int.sqf";
 fnc_randint = compile preprocessfile "randint\fnc_randint.sqf";
+fnc_shuffle = compile preprocessfile "randint\fnc_shuffle.sqf";
+fnc_choose = compile preprocessfile "randint\fnc_choose.sqf";
 #endif
