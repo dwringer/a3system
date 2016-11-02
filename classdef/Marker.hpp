bca476594ca26de303d5eed1fc064a98552686c2
Darren Ringer <DRinger@DARREN-POB.domain.pob>
2016-11-02 09:23:10 -0400

    marker docs

--- classdef/Marker.hpp
+++ classdef/Marker.hpp
@@ -1,6 +1,46 @@
+/*
+  Marker Class
+    :: Marker -> ObjectRoot
+
+  Methods:
+    __init__ position shape type size :: Create the Marker instance (hidden)
+    show                              :: Create and display the game marker
+    hide                              :: Remove the game marker from the map
+    redraw                            :: If marker is visible, recompose it
+    set_alpha alpha                   :: Set configured alpha [and redraw]
+    set_brush brush                   :: Set configured brush type [and redraw]
+    set_color color                   :: Set configured color [and redraw]
+    set_direction direction           :: Set configured direction [and redraw]
+    set_position position             :: Set configured position [and redraw]
+    set_shape shape                   :: Set configured shape [and redraw]
+    set_size size                     :: Set configured size [and redraw]
+    set_text text                     :: Set configured text [and redraw]
+    set_type type                     :: Set configured type [and redraw]
+
+      This class is an object wrapper for map marker objects (made with the
+  createMarker command).  These can be shown/hidden at will, and reconfigured
+  on the fly with immediate results if the marker is currently shown.  Each 
+  marker is given a unique name by using the value of global variable 
+  MarkerIndex and then incrementing it by one.  See the official documentation
+  for information on what parameters are supported.  In the initializer, type
+  may be passed as nil if drawing an area (i.e. "RECTANGLE", "ELLIPSE") marker.
+
+  Example:
+   mkr = ["Marker", position player, "ELLIPSE", nil, [25, 25]] call fnc_new;
+   [mkr, "show"] call fnc_tell;
+   // This puts a circle of 25m-diameter directly on the player's position in
+   //  the map.   
+  
+   [mkr, "set_color", "ColorGreen"] call fnc_tell;
+   // The marker is now green.
+
+*/
+
 MarkerIndex = 0;
 
+
 DEFCLASS("Marker") ["_self", "_position", "_shape", "_type", "_size"] DO {
+	/* Initialize marker parameters with the minimum required info */
 	SUPER("ObjectRoot", _self);
 	[_self, "_setf", "name",
 	 format ["MarkerInstance%1", MarkerIndex]] call fnc_tell;
@@ -14,60 +54,70 @@ DEFCLASS("Marker") ["_self", "_position", "_shape", "_type", "_size"] DO {
 
 
 DEFMETHOD("Marker", "set_alpha") ["_self", "_alpha"] DO {
+	/* Configure alpha (opacity) */
 	[_self, "_setf", "alpha", _alpha] call fnc_tell;
 	[_self, "redraw"] call fnc_tell;
 } ENDMETHOD;
 
 
 DEFMETHOD("Marker", "set_brush") ["_self", "_brush"] DO {
+	/* Configure brush (area fill type) */
 	[_self, "_setf", "brush", _brush] call fnc_tell;
 	[_self, "redraw"] call fnc_tell;
 } ENDMETHOD;
 
 
 DEFMETHOD("Marker", "set_color") ["_self", "_color"] DO {
+	/* Configure color (array or Cfg value) */
 	[_self, "_setf", "color", _color] call fnc_tell;
 	[_self, "redraw"] call fnc_tell;
 } ENDMETHOD;
 
 
 DEFMETHOD("Marker", "set_direction") ["_self", "_direction"] DO {
+	/* Set marker orientation (in degrees) */
 	[_self, "_setf", "direction", _direction] call fnc_tell;
 	[_self, "redraw"] call fnc_tell;
 } ENDMETHOD;
 
 
 DEFMETHOD("Marker", "set_position") ["_self", "_position"] DO {
+	/* Set marker position */
 	[_self, "_setf", "position", _position] call fnc_tell;
 	[_self, "redraw"] call fnc_tell;
 } ENDMETHOD;
 
 
 DEFMETHOD("Marker", "set_shape") ["_self", "_shape"] DO {
+	/* Set marker shape (i.e. "RECTANGLE", "ELLIPSE", "ICON") */
 	[_self, "_setf", "shape", _shape] call fnc_tell;
 	[_self, "redraw"] call fnc_tell;
 } ENDMETHOD;
 
 
 DEFMETHOD("Marker", "set_size") ["_self", "_size"] DO {
+	/* Set size [X x Y] */
 	[_self, "_setf", "size", _size] call fnc_tell;
 	[_self, "redraw"] call fnc_tell;
 } ENDMETHOD;
 
 
 DEFMETHOD("Marker", "set_text") ["_self", "_text"] DO {
+	/* Configure marker text */
 	[_self, "_setf", "text", _text] call fnc_tell;
 	[_self, "redraw"] call fnc_tell;
 } ENDMETHOD;
 
 
 DEFMETHOD("Marker", "set_type") ["_self", "_type"] DO {
+	/* Configure marker type (i.e. which icon) */
 	[_self, "_setf", "type", _type] call fnc_tell;
 	[_self, "redraw"] call fnc_tell;
 } ENDMETHOD;
 
 
 DEFMETHOD("Marker", "redraw") ["_self"] DO {
+	/* If shown, hide and then recreate the marker */
 	private ["_marker"];
 	_marker = [_self, "_getf", "marker"] call fnc_tell;
 	if (not isNil "_marker") then {
@@ -78,6 +128,7 @@ DEFMETHOD("Marker", "redraw") ["_self"] DO {
 
 
 DEFMETHOD("Marker", "show") ["_self"] DO {
+	/* Create the marker using the loaded configuration */
 	private ["_marker", "_name", "_position", "_shape", "_type", "_size",
                  "_brush", "_color", "_direction", "_text", "_alpha"];
 	_name = [_self, "_getf", "name"] call fnc_tell;
@@ -118,6 +169,7 @@ DEFMETHOD("Marker", "show") ["_self"] DO {
 
 
 DEFMETHOD("Marker", "hide") ["_self"] DO {
+	/* Remove marker from the map */
 	deleteMarker ([_self, "_getf", "name"] call fnc_tell);
 	[_self, "_setf", "marker", nil] call fnc_tell;
 } ENDMETHOD;
